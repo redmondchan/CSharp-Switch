4072125c6b3c603b4fd25ebae3fa89ee7546d9d3
Redmond Chan <redmondchan92@gmail.com>
2019-12-25 18:04:39 +0000

    Added example with default case

--- main.cs
+++ main.cs
@@ -2,20 +2,40 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+    
+    Console.WriteLine("Pick one of the following colors: green, red, yellow");
+    var color = Console.ReadLine();
 
-    int num = 3;
-
-    switch (num)
+    switch(color)
     {
-      case 1:
-        Console.WriteLine("Case 1");
+      case "green":
+        Console.WriteLine("You picked green");
         break;
-      case 2:
-        Console.WriteLine("Case 2");
+      case "yellow":
+        Console.WriteLine("You picked yellow");
         break;
-      case 3:
-        Console.WriteLine("Case 3");
+      case "red":
+        Console.WriteLine("You picked red");
+        break;
+      default: 
+        Console.WriteLine("You did not pick from the colors provided.");
         break;
     }
+
+    // example without default
+    // int num = 3;
+
+    // switch (num)
+    // {
+    //   case 1:
+    //     Console.WriteLine("Case 1");
+    //     break;
+    //   case 2:
+    //     Console.WriteLine("Case 2");
+    //     break;
+    //   case 3:
+    //     Console.WriteLine("Case 3");
+    //     break;
+    // }
   }
 }
