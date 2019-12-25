7afc52d826819f819eb9fbfd62c672d60092395f
Redmond Chan <redmondchan92@gmail.com>
2019-12-25 17:48:47 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    int num = 3;
+
+    switch (num)
+    {
+      case 1:
+        Console.WriteLine("Case 1");
+        break;
+      case 2:
+        Console.WriteLine("Case 2");
+        break;
+      case 3:
+        Console.WriteLine("Case 3");
+        break;
+    }
+  }
+}
