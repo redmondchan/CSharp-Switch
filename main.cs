91976dd6bca4d32c433b793c099a7315e9e43412
Redmond Chan <redmondchan92@gmail.com>
2019-12-25 18:21:10 +0000

    added example of break error

--- main.cs
+++ main.cs
@@ -39,5 +39,22 @@ class MainClass {
     //     break;
     // }
 
+    // ---------------------------------------------------
+
+    // example of error for not including break
+    // int num = 3;
+
+    // switch (num)
+    // {
+    //   case 1:
+    //     Console.WriteLine("Case 1");
+    //     break;
+    //   case 2:
+    //     Console.WriteLine("Case 2");
+    //     break;
+    //   case 3:
+    //     Console.WriteLine("Case 3");
+    //     break;
+    // }
   }
 }
